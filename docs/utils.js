dca65f7f51cc749381db9f28e83c57472db40142
Santiago Carmuega <santiago@carmuega.me>
2017-04-06 06:50:50 -0300

    Use querystring to get report guid

---
+++ docs/utils.js
@@ -0,0 +1,9 @@
+function parseQuery(qstr) {
+    var query = {};
+    var a = (qstr[0] === '?' ? qstr.substr(1) : qstr).split('&');
+    for (var i = 0; i < a.length; i++) {
+        var b = a[i].split('=');
+        query[decodeURIComponent(b[0])] = decodeURIComponent(b[1] || '');
+    }
+    return query;
+}
